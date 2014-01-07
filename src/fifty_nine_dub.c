d2db28af5fcab122c43fa726e69ce14b6dd6ee3f
Leigh Murray <github@leighmurray.com>
2014-01-07 18:10:51 +1100

    added out sent handlers

--- src/fifty_nine_dub.c
+++ src/fifty_nine_dub.c
@@ -312,6 +312,14 @@ static void load_date_format () {
   }
 }
 
+void out_sent_handler(DictionaryIterator *sent, void *context) {
+   // outgoing message was delivered
+}
+
+void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+  // outgoing message failed
+}
+
 void in_received_handler(DictionaryIterator *iter, void *context) {
   // incoming message received
 
@@ -451,6 +459,9 @@ static void init(void) {
 
   app_message_register_inbox_received(in_received_handler);
   app_message_register_inbox_dropped(in_dropped_handler);
+  app_message_register_outbox_sent(out_sent_handler);
+  app_message_register_outbox_failed(out_failed_handler);
+
   const uint32_t inbound_size = 64;
   const uint32_t outbound_size = 64;
   app_message_open(inbound_size, outbound_size);
